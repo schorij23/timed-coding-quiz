cf331591f1c1af144883754499ab3e895329bd0c
schorij23 <schorij23@gmail.com>
2023-09-18 23:04:26 -0400

    Ran out of time

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+
+const questions = [
+   {
+     text: "what is string?",
+     answers: [
+       'A) a number',
+       'B) a text character or word',
+       'C) a variable',
+       'D) a way to store objects',
+     ],
+     correct: "B",
+   },
+   {
+     text:
+       "What is a variable?",
+     answers: [
+       'A) a number',
+       'B) a text character',
+       'C) a countainer that holds a value',
+       'D) a question',
+     ],
+     correct: "C",
+   },
+   {
+     text:
+       "What is an Array used for in coding?",
+     answers: [
+      'A) Space staion energy storage', 
+      'B) A way to design draw multiple copies of a pattern',
+      'C) A way to store a list or colllection of items in a sing variable name',
+      'D) A way to store primative data types or values'],
+     correct: "C",
+   }];
+
+
 // my html element references
 var timerQz = document.getElementById("timer-count")
 var startBtn = document.getElementById('start-quiz')
@@ -8,9 +43,19 @@ var quizParts = {
    scores: document.getElementById('scores')
 }
 
+var nextBtn = document.getElementById('nextbtn')
+
 quizParts.theQuestion.style.setProperty('display', 'none');
 quizParts.yourScore.style.setProperty('display', 'none');
 quizParts.scores.style.setProperty('display', 'none');
+
+let questionNo;
+let questionAnswer;
+let timeLeft;
+let scores;
+
+
+
 
 function startQuiz() {
  
@@ -22,5 +67,6 @@ quizParts.testName.style.setProperty('display', 'none');
 quizParts.theQuestion.style.setProperty('display', 'block');
 }
 startBtn.addEventListener('click', startQuiz);
+
 
  
